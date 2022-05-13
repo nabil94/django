5086187ce4f96315e48bf1ba05d772a641f4a57f
root <root@ip-172-31-81-202.ec2.internal>
2022-05-13 10:28:57 +0000

    Fix Raise Custom Exceptions
    In file: another_bad_module.py,  an {Exception} or a {BaseException} was raised. This creates confusion since the exceptions are then differentiated only by the messages they carry. iCR suggested that a specific built-in exception or a custom exception is raised. For more information, read the Python guideline for raising exception.

--- tests/utils_tests/test_module/another_bad_module.py
+++ tests/utils_tests/test_module/another_bad_module.py
@@ -6,4 +6,6 @@ site._registry.update({
     'foo': 'bar',
 })
 
+# OpenRefactory Warning: Raising 'Exception' and 'BaseException' directly will have a negative impact on any code trying to catch these exceptions.
+# Raise a more specific built-in exception or, create a custom one.
 raise Exception('Some random exception.')
